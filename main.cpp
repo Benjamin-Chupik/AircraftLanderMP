6f1d0952cf7cdc7fd7a0346a281442baf1833df4
Benjamin-Chupik <BHChupik@gmail.com>
2022-12-05 09:28:50 -0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/*
+By: Benjamin chupik
+For algorithmic motion planning
+Main function for final project
+
+State Vector: [x, y, z, x_dot, y_dot, z_do, yaw, pitch, roll, yaw_dot, pitch_dot, roll_dot]
+Input Vector: [d_r, d_e, d_a] (ruder, elevator, aileron)
+
+*/
+
+//--------------------------------------------------------------------
+// Program Setup
+//--------------------------------------------------------------------
+//#include <ompl/config.h>
+//#include <ompl/base/spaces/SE3StateSpace.h>
+#include <iostream>
+
+// namespace ob = ompl::base;
+
+//--------------------------------------------------------------------
+// Support Functions
+//--------------------------------------------------------------------
+
+/*
+bool isStateValid(const ob::State *state)
+{
+}
+*/
+
+//--------------------------------------------------------------------
+// Main Run Function
+//--------------------------------------------------------------------
+
+int main(int /*argc*/, char ** /*argv*/)
+{
+    std::cout << "Starting Run" << std::endl;
+}
