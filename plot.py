869d6c62a6fad0f972d8bec90e571918b01b3c22
Benjamin-Chupik <BHChupik@gmail.com>
2022-12-08 10:12:05 -0700

    added 3d plotting, [broken] color

--- plot.py
+++ plot.py
@@ -5,24 +5,44 @@ Plots the generated path from the main.cpp run
 
 
 Data file structure:
-    [x_pos, y_pos, z_pos, x_quat, y_quat, z_quat, w_quat]
+    [x_pos, y_pos, z_pos, roll, pitch, yaw, x_dot, y_dot, z_dot, roll_dot, pitch_dot, yaw_dot]
 
 """
 
+from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import sqrt
 
 
 def main():
     # import path
     path = np.genfromtxt("./build/Debug/OutputPath.data", delimiter=" ")
+    x = path[:, 0]
+    y = path[:, 1]
+    z = path[:, 2]
+
+    roll = path[:, 3]
+    pitch = path[:, 4]
+    yaw = path[:, 5]
+
+    x_dot = path[:, 6]
+    y_dot = path[:, 7]
+    z_dot = path[:, 8]
+
+    roll_dot = path[:, 9]
+    pitch_dot = path[:, 10]
+    yaw_dot = path[:, 11]
 
     # Plotting
     fig = plt.figure()
     ax = plt.axes(projection="3d")
     fig.tight_layout()
 
-    ax.plot3D(path[:, 0], path[:, 1], path[:, 2])
+    # get color map for velocity
+    col = sqrt(x_dot**2+y_dot**2+z_dot**2)
+
+    ax.plot3D(x, y, z, c=col)
     ax.set_xlabel("x Position")
     ax.set_ylabel("y Position")
     ax.set_zlabel("z Position")
